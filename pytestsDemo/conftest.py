86f3ea3f3503e5f645e180ad3855d02cf53c9923
Shaima Anvar <shaima.anvar@gmail.com>
2024-12-06 20:06:18 -0500

    78.using fixtures to load data into tests

--- pytestsDemo/conftest.py
+++ pytestsDemo/conftest.py
@@ -1,8 +1,11 @@
 import pytest
-
-
-@pytest.fixture
-def setup(scope="class"):
+@pytest.fixture(scope='class')
+def setup():
     print("I will execute first")
     yield
     print("I will execute last")
+
+@pytest.fixture()
+def dataload():
+    print("user profile data being created")
+    return ["shaima","anvar","shaima@gmail.com"]
