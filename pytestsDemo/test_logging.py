b46b8dcd49f175bbbf5be27438b88351b29c62ab
Shaima Anvar <shaima.anvar@gmail.com>
2024-12-11 12:38:25 -0500

    81. Introduction to logging, 82. Importance of filehandler in logging tests

---
+++ pytestsDemo/test_logging.py
@@ -0,0 +1,8 @@
+import logging
+
+logger=logging.getLogger(__name__)
+logger.debug("A debug statement is executed")
+logger.info("Information")
+logger.warning("warning")
+logger.error("error")
+logger.critical("critical")
