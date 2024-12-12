63cc6d669405e8932a629ff3bf07cbb9ef46d69a
Shaima Anvar <shaima.anvar@gmail.com>
2024-12-11 19:10:05 -0500

    84. logging levels

--- pytestsDemo/test_logging.py
+++ pytestsDemo/test_logging.py
@@ -1,8 +1,13 @@
 import logging
-
-logger=logging.getLogger(__name__)
-logger.debug("A debug statement is executed")
-logger.info("Information")
-logger.warning("warning")
-logger.error("error")
-logger.critical("critical")
+def test_loggingDemo():
+    logger=logging.getLogger(__name__)
+    fileHandler=logging.FileHandler('logfile.log')
+    formatter=logging.Formatter("%(asctime)s :%(levelname)s :%(name)s :%(message)s")
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+    logger.setLevel(logging.CRITICAL)
+    logger.debug("A debug statement is executed")
+    logger.info("Information")
+    logger.warning("warning")
+    logger.error("error")
+    logger.critical("critical")
